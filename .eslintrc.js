1bafb9fcb2b1349c2027597959f4d1a679d20efe
Victor Batalha <madivcb@gmail.com>
2019-08-29 21:34:15 -0300

    feat: eslint, prettier and editor config setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,38 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+  env: {
+    es6: true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'import', 'prettier', 'jsx-a11y', 'react-hooks'],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/prefer-default-export': 'off',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
+    'react/jsx-one-expression-per-line': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-param-reassign': 'off',
+    'no-underscore-dangle': 'off',
+    'global-require': 'off',
+    'react-native/no-raw-text': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    camelcase: 'off',
+  },
+}; 
