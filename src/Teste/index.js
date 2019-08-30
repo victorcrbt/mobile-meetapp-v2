15bfeedb54c056de7c5fa17e9933082863670a92
Victor Batalha <madivcb@gmail.com>
2019-08-29 23:30:42 -0300

    feat: background setup

--- src/Teste/index.js
+++ src/Teste/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-// import { Container } from './styles';
+import Background from '~/components/Background';
 
 export default function Test() {
-  return <Text>Teste</Text>;
+  return (
+    <Background>
+      <Text>Teste</Text>
+    </Background>
+  );
 }
