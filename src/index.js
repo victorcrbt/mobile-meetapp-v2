c07259833087a0e6894381ddce6f41997359688a
Victor Batalha <madivcb@gmail.com>
2019-08-30 23:56:55 -0300

    feat: route protection setup

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import './config/ReactotronConfig';
+
+import App from './App';
+import { store, persistor } from './store';
+
+const Index = () => {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  );
+};
+
+export default Index;
