fb52a4c296ac95c9006ce7ab50b4e49015cf77e9
Victor Batalha <madivcb@gmail.com>
2019-08-31 00:47:09 -0300

    feat: header and status bar stylization

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Container, Logo } from './styles';
+
+import logo from '~/assets/logo.png';
+
+export default function Header() {
+  return (
+    <Container>
+      <Logo source={logo} />
+    </Container>
+  );
+}
