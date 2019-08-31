26b86f29bf406a93d687cf64f94ba482cd15b0f7
Victor Batalha <madivcb@gmail.com>
2019-08-30 23:44:07 -0300

    feat: condition api address

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
+import { Platform } from 'react-native';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL:
+    Platform.OS === 'ios' ? 'http://localhost:3333' : 'http://10.0.0.102:3333',
 });
 
 export default api;
