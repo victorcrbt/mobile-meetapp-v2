f3b89ec2cadde96a2c25734367d8c30e0caef462
Victor Batalha <madivcb@gmail.com>
2019-08-31 01:21:11 -0300

    fix: fix style issues

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -3,7 +3,6 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   width: 100%;
   height: 50px;
-  margin: 0 30px;
   margin-bottom: 10px;
   padding: 0 10px;
   border-radius: 5px;
