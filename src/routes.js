4060f1f66cd28c1a5919772684954c27f99b43c4
Victor Batalha <madivcb@gmail.com>
2019-08-31 14:18:06 -0300

    feat: list of subscriptions and cancel a subscription

--- src/routes.js
+++ src/routes.js
@@ -20,8 +20,8 @@ export default (isSigned = false) =>
         }),
         App: createBottomTabNavigator(
           {
-            Dashboard,
             Subscriptions,
+            Dashboard,
             Profile,
           },
           {
